7b583aa4826440623532f3afc9b93356fb764589
Julius Legaspi <julius.legaspi@mrpau.com>
2018-12-12 20:49:22 +0800

    Add error handling and more enchancement

--- windows/python-setup/python-exe.bat
+++ windows/python-setup/python-exe.bat
@@ -1,18 +1,24 @@
 
 @echo off
 
-SET VERSION=3.4.3
-SET PYTHON_32BIT="python-%VERSION%.msi"
-SET PYTHON_64BIT="python-%VERSION%.amd64.msi"
+Set version=3.4.3
+Set python32Bit="python-%version%.msi"
+Set python64Bit="python-%version%.amd64.msi"
 
 rem Execute python based on machine architecture.
 :Check_Architecture
-if /i "%processor_architecture%"=="x86" (
-    IF NOT DEFINED PROCESSOR_ARCHITEW6432 (
-        msiexec /i %PYTHON_32BIT% /passive
-    ) ELSE (
-        msiexec /i %PYTHON_64BIT% /passive
+If /i "%processor_architecture%"=="x86"
+(
+    If Not Defined Processor_Architew6432
+    (
+        msiexec /i %python32Bit% /passive
+    ) 
+    Else
+    (
+        msiexec /i %python64Bit% /passive
     )           
-) else (
-        msiexec /i %PYTHON_64BIT% /passive
+) 
+Else
+(
+    msiexec /i %python64Bit% /passive
 )
