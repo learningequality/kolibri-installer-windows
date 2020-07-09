e9346951947463ff34783bde5edaea207ff52570
David Canas <david@learningequality.org>
2020-07-09 13:33:26 -0700

    Stop preemtive creation of build_src

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -2,8 +2,6 @@
 
 set -eo pipefail
 
-mkdir build_src
-
 if [[ $LE_TRIGGERED_FROM_BUILD_ID ]]
 then
   echo "--- Downloading from triggered build"
@@ -11,7 +9,7 @@ then
   mv dist build_src
   if [[ -z $BUILDKITE_TRIGGERED_FROM_BUILD_ID ]]
   then
-    buildkite-agent annotate "This is a rebuild from a triggered build. Using parent's tarball" --style warning
+    buildkite-agent annotate "This is a rebuild from a triggered build. Using parent's WHL" --style warning
   fi
 fi
 
